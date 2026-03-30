4c03f38adbca50cba697af86e13aa6a3189b54f4
Phillip Thapa <1thapat@gmail.com>
2026-03-30 09:29:56 -0400

    add models and configure TypeScript build

---
+++ src/models/Bookmark.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const bookmarkSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    note: {
+      type: String,
+      trim: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Bookmark = model("Bookmark", bookmarkSchema);
+
+export default Bookmark;
